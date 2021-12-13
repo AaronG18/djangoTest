7675f7441f4fe8561aae8a851b01f341c4f5c14d
DeveloperGG <41569137+DeveloperGG@users.noreply.github.com>
2021-12-14 01:24:38 +0800

    # p65 add reverse&resolve to reversely get url info

--- index/views.py
+++ index/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
+from django.shortcuts import reverse
+from django.urls import resolve
 
 
 # Create your views here.
 def mydate(request, year, month, day):
+    args = ['2019', '12', '12']
+    # reverse -> resolve
+    result = resolve(reverse('index:mydate', arg=args))
+    print('kwargs: ', result.kwargs)
+    print('url_name: ', result.url_name)
+    print('namespace: ', result.namespace)
+    print('view_name: ', result.view_name)
+    print('app_name: ', result.app_name)
     return HttpResponse(str(year)+'/'+str(month)+'/'+str(day))
 
 
 def index(request):
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    kwargs = {'year': 2010, 'month': 2, 'day': 10}
+    args = ['2019', '12', '12']
+    # use reverse to generate address
+    print(reverse('index:mydate', args=args))
+    print(reverse('index:mydate', kwargs=kwargs))
+    return HttpResponse(reverse('index:mydate', args=args))
 
 
 # def index(request, month):
